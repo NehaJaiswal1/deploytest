8cff2c4da0ea9ca7ef585dc0f71bf200668357b4
Neha Jaiswal <nehajaiswal694@gmail.com>
2023-12-02 22:37:02 +0530

    port update

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -3,6 +3,7 @@ const { Sequelize  } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE, process.env.ROOT_PATH, process.env.PASSWORD, {
     host: 'localhost', 
     dialect: 'mysql',
+    port: 3306,
   });
 
 module.exports = { sequelize };
