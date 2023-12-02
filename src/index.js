6ebb566ca169683bc93c6ace532a734102a899ac
Neha Jaiswal <nehajaiswal694@gmail.com>
2023-12-02 22:13:02 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 require("dotenv/config");
 const express = require('express');
+const cors = require('cors');
 const { sequelize } = require('./dbconnection');
-var cors = require('cors')
-
-
-
 const app = express();
-const route = require('./route/route')
-const port = process.env.PORT || 3000;
+const route = require('./route/route');
+const port = 3000;
+
 app.use(express.json());
-app.use('/',route);
-app.use(cors())
+
+// Enable CORS
+app.use(cors());
+
+// Allow all origins (you may want to restrict this in a production environment)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
+app.use('/', route);
+
 app.listen(port, async () => {
   try {
     await sequelize.authenticate();
