932791f0641c6a6a355a9fd71d7f82b56544e499
Neha Jaiswal <nehajaiswal694@gmail.com>
2023-12-02 22:39:50 +0530

    connecting string

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -1,9 +1,17 @@
-const { Sequelize  } = require('sequelize');
+// const { Sequelize  } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.ROOT_PATH, process.env.PASSWORD, {
-    host: 'localhost', 
-    dialect: 'mysql',
-    port: 3306,
-  });
+// const sequelize = new Sequelize(process.env.DATABASE, process.env.ROOT_PATH, process.env.PASSWORD, {
+//     host: 'localhost', 
+//     dialect: 'mysql',
+//     port: 3306,
+//   });
 
-module.exports = { sequelize };
+// module.exports = { sequelize };
+
+
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('mysql://root:password@localhost:3306/database');
+
+module.exports = { sequelize };
