295b3dde76d41268ed9c02f22a8b322a70fd180e
Neha Jaiswal <nehajaiswal694@gmail.com>
2023-12-02 23:42:23 +0530

    add root

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -10,36 +10,36 @@
 
 
 
-// const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize('mysql://root:Neha@369@127.0.0.1:3306/database');
+const sequelize = new Sequelize('mysql://root:anmol@369@127.0.0.1:3306/database');
 
-// module.exports = { sequelize };
+module.exports = { sequelize };
 
 const { Sequelize  } = require('sequelize');
 
-const sequelize = new Sequelize.Sequelize(process.env.DATABASE, process.env.ROOT_PATH, process.env.PASSWORD, {
-  dialect: 'mysql',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  timezone: '+00:00',
-  define: {
-    charset: 'utf8mb4',
-    collate: 'utf8mb4_general_ci',
-    underscored: true,
-    freezeTableName: true,
-  },
-  pool: {
-    min: 0,
-    max: 5,
-  },
-  // logQueryParameters: NODE_ENV === 'development',
-  logging: (query, time) => {
-    logger.info(time + 'ms' + ' ' + query);
-  },
+// const sequelize = new Sequelize.Sequelize(process.env.DATABASE, process.env.ROOT_PATH, process.env.PASSWORD, {
+//   dialect: 'mysql',
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT),
+//   timezone: '+00:00',
+//   define: {
+//     charset: 'utf8mb4',
+//     collate: 'utf8mb4_general_ci',
+//     underscored: true,
+//     freezeTableName: true,
+//   },
+//   pool: {
+//     min: 0,
+//     max: 5,
+//   },
+//   // logQueryParameters: NODE_ENV === 'development',
+//   logging: (query, time) => {
+//     logger.info(time + 'ms' + ' ' + query);
+//   },
 
-  benchmark: true,
-});
+//   benchmark: true,
+// });
 
 
-module.exports = { sequelize };
+// module.exports = { sequelize };
