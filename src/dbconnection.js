a4526a86ed9f08fc61ca33a88745b17eae107339
Neha Jaiswal <nehajaiswal694@gmail.com>
2023-12-02 22:56:09 +0530

    host name

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -1,17 +1,17 @@
-// const { Sequelize  } = require('sequelize');
+const { Sequelize  } = require('sequelize');
 
-// const sequelize = new Sequelize(process.env.DATABASE, process.env.ROOT_PATH, process.env.PASSWORD, {
-//     host: 'localhost', 
-//     dialect: 'mysql',
-//     port: 3306,
-//   });
+const sequelize = new Sequelize(process.env.DATABASE, process.env.ROOT_PATH, process.env.PASSWORD, {
+    host: 'mysql', 
+    dialect: 'mysql'
+    // port: 3306,
+  });
 
-// module.exports = { sequelize };
+module.exports = { sequelize };
 
 
 
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('mysql://root:Neha@369@127.0.0.1:3306/database');
+// const sequelize = new Sequelize('mysql://root:Neha@369@127.0.0.1:3306/database');
 
-module.exports = { sequelize };
+// module.exports = { sequelize };
